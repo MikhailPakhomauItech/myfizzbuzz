f172548796c41cf1f82c88d4e40d0e7510fb5214
zz <zzz>
2021-11-18 11:03:03 +0100

    add FizzBuzzService tests

--- src/main/java/com/demo/FizzBuzzService.java
+++ src/main/java/com/demo/FizzBuzzService.java
@@ -1,24 +1,34 @@
 package com.demo;
 
+import java.io.PrintStream;
+
 public class FizzBuzzService {
-  public void fizzBuzzIt() {
+  public void fizzBuzzIt(PrintStream out, int rate) {
+    if (out == null) {
+      throw new IllegalArgumentException("PrintStream should be not a null!");
+    }
+
     StringBuilder sb = new StringBuilder();
-    for (int i = 1; i <= 100; i++) {
-      if (0 == i % 3) {
-        if (0 == i % 5) {
-          sb.append("FizzBuzz");
-        } else {
-          sb.append("Fizz");
-        }
-      } else if (0 == i % 5) {
-        sb.append("Buzz");
-      } else {
-        sb.append(i);
-      }
-      if (i != 100) {
+    for (int i = 1; i <= rate; i++) {
+      sb.append(x(i));
+      if (i != rate) {
         sb.append(" ");
       }
     }
-    System.out.println(sb);
+    out.print(sb);
+  }
+
+  String x(int i) {
+    if (i % 3 == 0) {
+      if (i % 5 == 0) {
+        return "FizzBuzz";
+      } else {
+        return "Fizz";
+      }
+    } else if (i % 5 == 0) {
+      return "Buzz";
+    } else {
+      return String.valueOf(i);
+    }
   }
 }
--- src/test/java/com/demo/FizzBuzzServiceTest.java
+++ src/test/java/com/demo/FizzBuzzServiceTest.java
@@ -1,17 +1,46 @@
 package com.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 class FizzBuzzServiceTest {
 
   FizzBuzzService fbs = new FizzBuzzService();
 
+  @Test
+  @DisplayName("Should return a string with proper values and without a space at the end of the row")
+  void fizzBuzzIt() throws UnsupportedEncodingException {
+    final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    try (PrintStream ps = new PrintStream(buffer, true, UTF_8.name())) {
+      fbs.fizzBuzzIt(ps, 20);
+    }
+    String result = buffer.toString(UTF_8.name());
+    String expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz";
+    assertEquals(expected, result);
+  }
 
   @Test
-  void fizzBuzzIt() {
-    fbs.fizzBuzzIt();
+  @DisplayName("Should fail with IllegalArgumentException because got a null instead of a buffer as argument")
+  void fizzBuzzItGetNullInsteadOfBuffer() {
+    assertThrows(IllegalArgumentException.class, () -> fbs.fizzBuzzIt(null, 20));
+  }
+
+  @Test
+  @DisplayName("Should not fail with negative range and just return an empty row")
+  void fizzBuzzItGetNegativeRange() throws UnsupportedEncodingException {
+    final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    try (PrintStream ps = new PrintStream(buffer, true, UTF_8.name())) {
+      fbs.fizzBuzzIt(ps, -1);
+    }
+    String result = buffer.toString(UTF_8.name());
+    assertEquals("", result);
   }
 }
