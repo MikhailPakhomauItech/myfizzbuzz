a8bc25ee1f4481e6df41270c653abd011755c08d
zz <zzz>
2021-11-18 09:59:37 +0100

    add Junit lib

---
+++ src/test/java/com/demo/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+  @Test
+  void testLibWorkGood() {
+    assertEquals(2, 2);
+  }
+
+}
